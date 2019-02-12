e293a3ca296898e34ed2ae8638fb7fe0fe505b1c
barischrooneyj <barischrooneyj@gmail.com>
2019-02-12 14:54:49 +0100

    Generate 1.1 images and videos

--- vibrating.py
+++ vibrating.py
@@ -3,54 +3,90 @@ import matplotlib.collections as mcoll
 import matplotlib.pyplot as plt
 import numpy as np
 
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, writers
 
-def Phifunction(x):
-    return math.sin(2 * math.pi * x)
 
-L = 1
-N = 500
-endTime = 800
-c = 1
-deltaT = 0.001
-frameSkip = 5
+def getStringMatrix(L=1, N=500, endTime=800, c=1, deltaT=0.001,
+                    phi_func=lambda x: math.sin(2 * math.pi * x)):
+    """A matrix, a wave amplitude at each position at each time step."""
 
-# Setup a string matrix, a wave amplitude at each position at each time step.
-# Initially we have only two time steps with values determined from t=0.
-stringMatrix = []
-startString = [0] * (N+1)
+    stringMatrix = []
+    startString = [0] * (N+1)
 
-for i in range(N+1):
-    if (i == 0 or i == N):
-        pass
-    else:
-        startString[i] = Phifunction(L/N*i)
-
-stringMatrix.append(startString)
-stringMatrix.append(startString)
-
-# For the remaining time steps calculate wave amplitude at each position.
-for j in range(endTime):
-    tempString = [0] * (N+1)
-    for i in range(N + 1):
-        if(i == 0 or i == N):
-            tempString[i] = 0
+    for i in range(N+1):
+        if (i == 0 or i == N):
+            pass
         else:
-            tempString[i] = (
-                (deltaT * c / (L/N) ) ** 2
-                * (stringMatrix[-1][i+1] + stringMatrix[-1][i-1]
-                   - 2 * stringMatrix[-1][i])
-                - stringMatrix[-2][i] + 2 * stringMatrix[-1][i])
-    stringMatrix.append(tempString)
+            startString[i] = phi_func(L/N*i)
 
-# Plot a repeating animation of the wave over time.
-x = range(N + 1)
-line, = plt.plot(x, stringMatrix[0])
-def animate(i):
-    global line
-    line.remove()
-    line, = plt.plot(x, stringMatrix[i])
-ani = FuncAnimation(
-    plt.gcf(), animate, frames=range(0, len(stringMatrix), frameSkip),
-    interval=1)
-plt.show()
+    # Initially we have only two time steps with values determined from t=0.
+    stringMatrix.append(startString)
+    stringMatrix.append(startString)
+
+    # For the remaining time steps calculate wave amplitude at each position.
+    for j in range(endTime):
+        tempString = [0] * (N+1)
+        for i in range(N + 1):
+            if(i == 0 or i == N):
+                tempString[i] = 0
+            else:
+                tempString[i] = (
+                    (deltaT * c / (L/N) ) ** 2
+                    * (stringMatrix[-1][i+1] + stringMatrix[-1][i-1]
+                    - 2 * stringMatrix[-1][i])
+                    - stringMatrix[-2][i] + 2 * stringMatrix[-1][i])
+        stringMatrix.append(tempString)
+
+    return stringMatrix
+
+
+def animate(stringMatrix, N=500, frameSkip=5):
+    """Plot a repeating animation of the wave over time."""
+
+    x = range(N + 1)
+    line, = plt.plot(x, stringMatrix[0])
+    def animate(i):
+        nonlocal line
+        line.remove()
+        line, = plt.plot(x, stringMatrix[i])
+    ani = FuncAnimation(
+        plt.gcf(), animate, frames=range(0, len(stringMatrix), frameSkip),
+        interval=1)
+    return ani
+
+
+if __name__ == "__main__":
+    titles_and_phi_funcs = [
+        ("1-1-B-i", "sin(2πx)", lambda x: math.sin(2 * math.pi * x)),
+        ("1-1-B-ii", "sin(5πx)", lambda x: math.sin(5 * math.pi * x)),
+        ("1-1-B-iii", "sin(5πx) if 1/5 < x < 2/5 else 0",
+         lambda x: math.sin(5 * math.pi * x) if (0.2 < x and 0.4 > x) else 0)
+    ]
+
+    # Generate a plot for each of the three setups.
+    time_fractions = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
+    # For each Phi function.
+    for (filename, title, phi_func) in titles_and_phi_funcs:
+        stringMatrix = getStringMatrix(phi_func=phi_func)
+        # Plot for each time step.
+        for time_fraction in time_fractions:
+            time_step = int((len(stringMatrix[0]) - 1) * time_fraction)
+            plt.plot(
+                range(len(stringMatrix[0])),
+                stringMatrix[time_step],
+                label=str(time_fraction)
+            )
+        plt.legend()
+        plt.title(title)
+        plt.savefig(filename + ".png", bbox_inches="tight")
+        plt.close()
+
+    Writer = writers['ffmpeg']
+    writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+    # For each Phi function generate an animation.
+    for (filename, title, phi_func) in titles_and_phi_funcs:
+        ani = animate(getStringMatrix(phi_func=phi_func))
+        ani.save(filename + ".mp4", writer=writer)
+        print("Saved")
