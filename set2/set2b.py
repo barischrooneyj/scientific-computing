a4b682663861c22c67724e1bd9103c44a11a04ea
barischrooneyj <barischrooneyj@gmail.com>
2019-02-27 14:52:04 +0100

    Add prob stick 2c

--- set2/set2b.py
+++ set2/set2b.py
@@ -3,12 +3,12 @@ import numpy as np
 # Top row i = 0.
 # Left column j = 0.
 
-DEBUG = True
+PRINT = True
 
 
 def p(s):
-    """Print if DEBUG is true."""
-    if DEBUG:
+    """Print if PRINT is true."""
+    if PRINT:
         print(s)
 
 
@@ -20,7 +20,7 @@ def initialWalkerMatrix(matrix_len, initial_walker):
     return matrix
 
 
-def newWalker(matrix):
+def newWalker(matrix, prob_stick=1):
     """Walk until a walker reaches the cluster.
 
     Initial walker position is a random cell in the top row. If a walker
@@ -50,28 +50,61 @@ def newWalker(matrix):
     # Create a new walker until one reaches the cluster.
     while True:
         i, j = (0, np.random.randint(matrix_len))
-        p("New walker at position ({}, {})".format(i, j))
+        p("Walker starting at ({}, {})".format(i, j))
         # Walk until out of bounds or cluster reached.
         while not outOfBounds(i):
-            if atCluster(i, j):
-                p("New walker at ({}, {})".format(i, j))
+            if (atCluster(i, j)
+                and (prob_stick >= 1 or np.random.uniform() < prob_stick)):
+                p("Walker joined cluster at ({}, {})".format(i, j))
                 return (i, j)
             i, j = newPosition(i, j)
-            p("New position = ({}, {})".format(i, j))
+            p("Walker moved to = ({}, {})".format(i, j))
 
 
-def walkerSimulation(matrix_len, initial_walker, max_walkers):
+def walkerSimulation(matrix_len, initial_walker, max_walkers, prob_stick,
+                     save=True, load=False):
     """Return a matrix after a random walker simulation."""
-    p("Initial walker = {}".format(initial_walker))
+    fname = "len-{}-init-{}-max-{}-ps-{}.csv".format(
+        matrix_len, initial_walker, max_walkers, prob_stick)
+
+    # Load and return previous results if possible.
+    if load:
+        try:
+            with open(fname) as f:
+                print("Loaded matrix from {}".format(fname))
+                return np.loadtxt(f)
+        except FileNotFoundError:
+            print("Could not load matrix from {}".format(fname))
+
     matrix = initialWalkerMatrix(matrix_len, initial_walker)
     p("Initial walker matrix = \n{}".format(matrix))
     for t in range(max_walkers):
         p("Looking for walker at time {}".format(t))
-        i, j = newWalker(matrix)
+        i, j = newWalker(matrix, prob_stick)
         matrix[i][j] = True
         p("Matrix after time {} =\n{}".format(t, matrix))
+
+    # Save the result
+    if save:
+        with open(fname, "w") as f:
+            np.savetxt(f, matrix)
+        print("Saved matrix to {}".format(fname))
+
     return matrix
 
 
 if __name__ == "__main__":
-    walkerSimulation(20, (-1, 9), 100)
+    matrix_len = 100
+    initial_walker = (-1, 49)
+    max_walkers = 1000
+    probs_stick = [0.1, 0.3, 0.5, 0.7, 0.9, 1]
+
+    for prob_stick in probs_stick:
+        matrix = walkerSimulation(
+            matrix_len,
+            initial_walker,
+            max_walkers,
+            prob_stick=prob_stick,
+            save=True,
+            load=False
+        )
