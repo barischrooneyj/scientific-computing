d8a780b1d41cf2cab078b02c5b2f40d2db70c488
barischrooneyj <barischrooneyj@gmail.com>
2019-03-17 13:05:04 +0100

    Update 3D plot

--- set3/set3a.py
+++ set3/set3a.py
@@ -1,4 +1,6 @@
 import heapq
+import json
+import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,14 +8,13 @@ import numpy as np
 
 def makeMatrixM(Ni, Nj, boundary_func):
     M = np.zeros((Ni * Nj, Ni * Nj))
-    print(M)
 
     for i in range(Ni):
         for j in range(Nj):
-            print("i, j = {}".format(i, j))
+            # print("i, j = {}".format(i, j))
             if not boundary_func(i, j, Ni, Nj):
 
-                print(i , j , "Special")
+                # print(i , j , "Special")
                 M[i * Ni + j, i * Ni + j] = -4
                 M[i * Ni + j, (i + 1) * Ni + j] = 1
                 M[i * Ni + j, (i - 1) * Ni + j] = 1
@@ -61,33 +62,71 @@ def plotMatrixM(M):
     plt.show()
 
 
-if __name__ == "__main__":
+def plotSpectrumOfEigenFrequencies(
+        Nis=np.arange(9, 80, 10), load=True, save=True, show=True):
+    """Plot eigenfrequencies while varying discretization step."""
+    print("Nis = {}".format(Nis))
     L = 1
-    Ni = 21
-    Nj = 21
-    dx = L / Ni
-    dy = L / Nj
+    eigenFrequencies = []
+    for Ni in Nis:
+        print("Ni = {}".format(Ni))
+        fname = "data/eigenfrequencies-{}.pickle".format(Ni)
+        loaded = False
+        if load:
+            try:
+                with open(fname, "rb") as f:
+                    eigenvalues = pickle.load(f)
+                loaded = True
+            except:
+                print("Could not load {}".format(fname))
+        if not loaded:
+            M = makeMatrixM(Ni + 2, Ni + 2, boundary)
+            dx = L / Ni
+            answer = np.linalg.eig(M * 1/dx**2)
+            eigenvalues = np.sort([x for x in answer[0] if x != 0])
+        eigenFrequencies.append(eigenvalues)
+        if save:
+            with open(fname, "wb") as f:
+                pickle.dump(eigenvalues, f)
+    plt.boxplot(eigenFrequencies, labels=[str(x) for x in Nis])
+    # plt.yscale("log")
+    plt.title(
+        "Eigenfrequencies varying discretization step")
+    if save:
+        plt.savefig("results/eigen-frequencies-{}.png".format(Nis).strip())
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Question D.
+    plotSpectrumOfEigenFrequencies(load=True, save=True)
+
+    # L = 1
+    # Ni = 21
+    # Nj = 21
+    # dx = L / Ni
+    # dy = L / Nj
 
     # M = makeMatrixM(Ni + 2, Nj + 2, boundary)
     # plotMatrixM(M)
     # M = makeMatrixM(Ni + 2, 2 * Nj + 2, boundary)
     # plotMatrixM(M)
-    M = makeMatrixM(Ni + 2, Nj + 2, circleBoundary)
-    plotMatrixM(M)
+    # M = makeMatrixM(Ni + 2, Nj + 2, circleBoundary)
+    # plotMatrixM(M)
 
-    v = np.zeros((Nj * Ni, 1))
+    # v = np.zeros((Nj * Ni, 1))
 
-    answer = np.linalg.eig(M * 1/dx**2)
-    eigenvalues = [x for x in answer[0] if x != 0]
-    print(eigenvalues)
-    eigenvectors = answer[1].T
-    smallest = smallest_eigenvalues(eigenvalues, n=10)
+    # answer = np.linalg.eig(M * 1/dx**2)
+    # eigenvalues = [x for x in answer[0] if x != 0]
+    # print(eigenvalues)
+    # eigenvectors = answer[1].T
+    # smallest = smallest_eigenvalues(eigenvalues, n=10)
 
-    for i, eigenvalue in smallest:
-        eigenvector = eigenvectors[i]
-        eigenmatrix = eigenvector.reshape(Ni+2, Nj+2)
-        plt.imshow(eigenmatrix.real)
-        plt.show()
+    # for i, eigenvalue in smallest:
+    #     eigenvector = eigenvectors[i]
+    #     eigenmatrix = eigenvector.reshape(Ni+2, Nj+2)
+    #     plt.imshow(eigenmatrix.real)
+    #     plt.show()
         # plt.plot(list(range(len(eigenvector))), eigenvector)
         # plt.show()
 
