1b2b7cce4d0dd2889940e47dd27a934f22659819
barischrooneyj <barischrooneyj@gmail.com>
2019-03-23 15:18:02 +0100

    Subplots

--- set3/set3a.py
+++ set3/set3a.py
@@ -13,24 +13,17 @@ def makeMatrixM(Ni, Nj, boundary_func):
     for i in range(Ni):
         for j in range(Nj):
             if not boundary_func(i, j, Ni, Nj):
-                M[i * Ni + j, i * Ni + j] = -4
-                M[i * Ni + j, (i + 1) * Ni + j] = 1
-                M[i * Ni + j, (i - 1) * Ni + j] = 1
-                M[i * Ni + j, i * Ni + (j + 1)] = 1
-                M[i * Ni + j, i * Ni + (j - 1)] = 1
-
+                M[i * Nj + j, i * Nj + j] = -4
+                M[i * Nj + j, (i + 1) * Nj + j] = 1
+                M[i * Nj + j, (i - 1) * Nj + j] = 1
+                M[i * Nj + j, i * Nj + (j + 1)] = 1
+                M[i * Nj + j, i * Nj + (j - 1)] = 1
     return M
 
 
 def boundary(i, j, Ni, Nj):
-    if i == 0 or i == Ni - 1:
-        return True
-
-    elif j == 0 or j == Nj - 1:
-        return True
-
-    else:
-        return False
+    return (i == 0 or i == Ni - 1
+           or j == 0 or j == Nj - 1)
 
 
 def circleBoundary(i, j, Ni, Nj):
@@ -107,7 +100,8 @@ def plot_spectrum_of_eigen_frequencies(
         plt.show()
 
 
-def plot_eigenvectors_for_shapes(Ni_=29, Nj_=29, save=True, show=True):
+def plot_eigenvectors_for_shapes(Ni_=29, Nj_=29, save=True, show=True,
+                                 plots_per_subplot=4):
     """3B: Plot eigenvectors for the smallest eigenvalues for each shape."""
     if Ni_ % 2 == 0:
         raise ValueError("Ni should be an odd number")
@@ -118,37 +112,78 @@ def plot_eigenvectors_for_shapes(Ni_=29, Nj_=29, save=True, show=True):
     dx = L / Ni_
     dy = L / Nj_
 
+    # For each system shape.
     for shape, Ni, Nj, boundary_f in [
         ("square",      Ni_, Nj_,   boundary),
         ("rectangular", Ni_, 2*Nj_, boundary),
         ("circular",    Ni_, Nj_,   circleBoundary)
     ]:
         M = makeMatrixM(Ni + 2, Nj + 2, boundary_f)
+        plt.close()
         plotMatrixM(M, Ni, Nj, save=False, show=show)
 
         eigenvalues_and_eigenvectors = np.linalg.eig(M * 1/dx**2)
         eigenvalues = [x for x in eigenvalues_and_eigenvectors[0] if x != 0]
         eigenvectors = eigenvalues_and_eigenvectors[1].T
-        smallest_eigenvalues = get_smallest_eigenvalues(eigenvalues, n=10)
+        smallest_eigenvalues = get_smallest_eigenvalues(eigenvalues, n=20)
 
-        for i, eigenvalue in smallest_eigenvalues:
+        eig_names = set()
+        def notyet(i, eigenvalue):
+            """Eigenvalue not yet seen with same leading integer part?"""
+            eig_name = str(eigenvalue).split(".", 1)[0]
+            if eig_name in eig_names:
+                return False
+            eig_names.add(eig_name)
+            return True
+
+        # Remove duplicate eigenvalues.
+        smallest_eigenvalues = [x for x in smallest_eigenvalues if notyet(*x)]
+
+        plt.close()
+        # For each of n smallest eigenvalues.
+        for plt_num, (i, eigenvalue) in enumerate(smallest_eigenvalues):
+
+            # Create a new figure for the subplots.
+            if plt_num % plots_per_subplot == 0:
+                fig, axes = plt.subplots(plots_per_subplot, 1)
+
+            # Reshape eigenvector.
             eigenvector = eigenvectors[i]
             eigenmatrix = eigenvector.reshape(Ni + 2, Nj + 2)
-            print("Eigenvalue {} for shape {} eigenvector = \n{}".format(
-                eigenvalue, shape, eigenvector.tolist()))
-            plt.imshow(eigenmatrix.real)
-            plt.ylabel("Row index i")
-            plt.xlabel("Column index j")
-            plt.title(
-                "Eigenvector (reshaped) for eigenvalue"
-                + "\n{:.2f} for a {} x {} {} system".format(
-                    eigenvalue, Ni, Nj, shape)
-            )
-            if save:
-                plt.savefig("results/3b-eigenvector-shape-{}-eigenvector{:.2f}".format(
-                    shape, eigenvalue).replace(".", "-") + ".png")
-            if show:
-                plt.show()
+
+            # Save eigenvector to file.
+            fname = "eigenvectors/shape-{}-{}-x-{}-eigenvalue-{}.json".format(
+                shape, Ni, Nj, eigenvalue)
+            print(("Eigenvector for eigenvalue {}\nfor shape {} " +
+                   "system size {} x {} saved to:\n\t{}").format(
+                       eigenvalue, shape, Ni, Nj, fname))
+            with open(fname, "w") as f:
+                json.dump(eigenvector.tolist(), f)
+
+            # Plot subplot.
+            ax = axes[plt_num % plots_per_subplot]
+            ax.imshow(eigenmatrix.real)
+            ax.set_ylabel("Index k")
+            ax.set_xlabel("Index l")
+            ax.set_title("λ = {:.2f}".format(eigenvalue))
+
+            # Save/show figure.
+            if plt_num % plots_per_subplot == plots_per_subplot - 1:
+
+                # TODO: Set subplot width.
+                # fig.set_figwidth(5)
+                # plt.subplots_adjust(wspace=0)
+
+                plt.suptitle(("Reshaped eigenvector for a\n " +
+                              "{} system on a {} x {} grid").format(
+                                    shape, Ni + 2, Nj + 2))
+                plt.tight_layout()
+                if save:
+                    plt.savefig(("results/3b-eigenvector-" +
+                                "shape-{}-eigenvector{:.2f}").format(
+                                shape, eigenvalue).replace(".", "-") + ".png")
+                if show:
+                    plt.show()
 
 
 def animation_of_membrane(v0, K, A=1, B=1, c=1, dt=0.01, max_t=6.4,
@@ -200,10 +235,10 @@ if __name__ == "__main__":
     # plotMatrixM(M, Ni, Nj, fstart="3a-", show=True)
 
     # Question B.
-    # plot_eigenvectors_for_shapes(show=True)
+    plot_eigenvectors_for_shapes(Ni_=29, Nj_=29, show=True)
 
     # Question D.
-    # plot_spectrum_of_eigen_frequencies(load=True, save=True, show=True)
+    plot_spectrum_of_eigen_frequencies(load=True, save=True, show=True)
 
     # Question E.
     import e_params
